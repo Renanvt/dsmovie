7114c5dce71e01e5993edfb9ae1a7f5404d16e7f
Renanvt <jonatan_rvs@hotmail.com>
2022-01-16 11:18:33 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,8 @@
-import { ReactComponent as GithubIcon } from './assets/img/github.svg';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <header>
-      <nav>
-        <div>
-          <h1>DSMovie</h1>
-          <a href="https://github.com/renanvt/dsmovie">
-            <div>
-              <GithubIcon />
-              <p>/devsuperior</p>
-            </div>
-          </a>
-        </div>
-      </nav>
-    </header>
+    <Navbar />
   );
 }
 
