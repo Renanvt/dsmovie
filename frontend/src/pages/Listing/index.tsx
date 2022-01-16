b584e370fb4c93be443512750503316eafb6a15f
Renanvt <jonatan_rvs@hotmail.com>
2022-01-16 12:33:22 -0300

    MovieCard 1

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,15 @@
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 
-function Listing(){
-    return(
-        <Pagination/>
-    );
+function Listing() {
+    return (
+        //A função só pode exportar 1 componente, então foi colocado dentro do div ou fragment <>
+        <>
+            <Pagination />
+            <MovieStars />
+            
+        </>
+      );
 }
 
 export default Listing;
