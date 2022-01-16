9a1cc27bbeede1c7c89d4f2d57e6dad3a752b5c2
Renanvt <jonatan_rvs@hotmail.com>
2022-01-16 13:38:43 -0300

    MovieCard2

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,43 @@
-import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
     return (
         //A função só pode exportar 1 componente, então foi colocado dentro do div ou fragment <>
+
+        // className="row" organiza os componentes em linha e "col" em coluna
+
+        //Por padrão o col total é 12
+
+        //div className="col-sm-6"
+        /*Esse div irá ocupar 6 colunas das 12 colunas do grid system do bootstrap ou seja cada card irá ocupar metade do meu container do bootstrap */
+
+        /*Bootstrap Breakpoints */
+
+        /*A partir da largura lg que é 992px, cada card vai ocupar 4 das 12 colunas, resultando em 3 cards por linha*/
         <>
             <Pagination />
-            <MovieScore />
-            
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
         </>
-      );
+    );
 }
 
 export default Listing;
