ee459b804c65d66dcd01a49709eecd8fe0f1aea7
Renanvt <jonatan_rvs@hotmail.com>
2022-01-19 18:42:19 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,25 +7,35 @@ import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    const [pageNumber, setPageNumber ] = useState(0);
+    const [pageNumber, setPageNumber] = useState(0);
+
+    //Estado pra guardar no componente a página que foi carregada
+
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true, //informa se é a ultima página
+        totalPages: 0,
+        totalElements: 0,
+        size: 12, //tamanho da página
+        number: 0, //número da página que eu estou
+        first: true, //È a primeira página ?
+        numberOfElements: 0,
+        empty: true
+    });
 
     useEffect(() => { //Executa a função somente na hora que carregar o componente, não executando mais de uma vez. Estou colocando a função dentro de um hook, amarrando a execução dentro do ciclo de vida do componente
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
-        .then(response => {
-            const data = response.data as MoviePage;
-            console.log(data);
-            setPageNumber(data.number); 
-            //imprime na tela o page number
+        //Garantir que os dados vão retornar a busca sempre na mesma ordem, coloque o parâmetro de ordenação &sort=id
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage; //Pega o corpo da resposta como sendo um MoviePage
+                setPage(data);
+                //O resultado dessa requisição é uma promisse
+                //.get é uma operação assíncrona
+                //Quando chegar a resposta, então (.then)
+            });
+    }, [pageNumber]); //Quando mudar o pageNumber eu vou fazer a requisição novamente
 
-            
-    //O resultado dessa requisição é uma promisse
-    //.get é uma operação assíncrona
-    //Quando chegar a resposta, então (.then)
-        });
-    }, []);
     /*O primeiro argumento do useEffect recebe uma função para ser executada, o segundo argumento é uma lista de objetos a ser observados. Sempre que alterar algo nos objetos, executa a função novamente */
-    // FORMA ERRADA
-   
 
 
     return (
@@ -42,25 +52,15 @@ function Listing() {
 
         /*A partir da largura lg que é 992px, cada card vai ocupar 4 das 12 colunas, resultando em 3 cards por linha*/
         <>
-        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </>
