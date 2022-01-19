3e7e87f50316dae359036e51b5eb093e22bc6964
Renanvt <jonatan_rvs@hotmail.com>
2022-01-19 18:18:51 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,31 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { MoviePage } from "types/movie";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data);
-    });
+    const [pageNumber, setPageNumber ] = useState(0);
+
+    useEffect(() => { //Executa a função somente na hora que carregar o componente, não executando mais de uma vez. Estou colocando a função dentro de um hook, amarrando a execução dentro do ciclo de vida do componente
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number); 
+            //imprime na tela o page number
+
+            
     //O resultado dessa requisição é uma promisse
     //.get é uma operação assíncrona
     //Quando chegar a resposta, então (.then)
+        });
+    }, []);
+    /*O primeiro argumento do useEffect recebe uma função para ser executada, o segundo argumento é uma lista de objetos a ser observados. Sempre que alterar algo nos objetos, executa a função novamente */
+    // FORMA ERRADA
+   
 
 
     return (
@@ -29,6 +42,7 @@ function Listing() {
 
         /*A partir da largura lg que é 992px, cada card vai ocupar 4 das 12 colunas, resultando em 3 cards por linha*/
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
